b3f77cfa94ae12a23167cece6123c8519b25a4ee
Gee-Lyle Wong <gee-lyle.wong@kcl.ac.uk>
2025-01-21 14:37:34 +0000

    feat: Added conversion of XML result to a dictionary of results.

--- fetch_data.py
+++ fetch_data.py
@@ -1,5 +1,6 @@
 import urllib
 import urllib.request
+import xmltodict
 
 if __name__ == "__main__":
 
@@ -10,5 +11,26 @@ if __name__ == "__main__":
     url = f'http://export.arxiv.org/api/query?search_query={search_query}&start={start}&max_results={max_results}&sortBy=relevance&sortOrder=descending'
     data = urllib.request.urlopen(url)
     result = data.read().decode("utf-8")
-    print(result)
-    print(type(result))
+
+    # Convert the XML result into a dictionary of the results
+    result = xmltodict.parse(result)
+    entries = []
+    for entry in result["feed"]["entry"]:
+        paper_data = {
+            "id": entry["id"],
+            "title": entry["title"],
+            "summary": entry["summary"].strip(),
+            "authors": [author["name"] for author in entry["author"]],
+            "published": entry["published"],
+            "pdf_link": next(link["@href"] for link in entry["link"] if link.get("@title") == "pdf")
+        }
+        entries.append(paper_data)
+    
+    for paper in entries:
+        print("ID:", paper["id"])
+        print("Title:", paper["title"])
+        print("Summary:", paper["summary"])
+        print("Authors:", paper["authors"])
+        print("Published:", paper["published"])
+        print("PDF Link:", paper["pdf_link"])
+        print("\n")
