5f6829ae7c9b4dde74cf6f8cdbe5c999f9a39b26
Gee-Lyle Wong <gee-lyle.wong@kcl.ac.uk>
2025-01-23 21:48:57 +0000

    docs: Added documentation for the 'TextPreprocessor' class methods.

--- src/data_processing/text_preprocessor.py
+++ src/data_processing/text_preprocessor.py
@@ -6,16 +6,42 @@ nltk.download('wordnet')
 
 class TextPreprocessor:
     def remove_newlines(self, text:str) -> str:
+        """
+        Removes whitespace characters (spaces, tabs, newlines, etc.) 
+        
+        Args:
+            text (str): The text to remove whitespace characters from.
+        """
         return re.sub(r'\s+', ' ', text)
     
     def remove_links(self, text:str) -> str:
+        """
+        Removes any URLs from the text.
+
+        Args:
+            text (str): The text to remove URLs from.
+        """
         pattern = r"http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
         return re.sub(pattern, '',text)
     
     def keep_only_alphanumeric(self, text:str) -> str:
+        """
+        Removes any characters that are not a letter, digit or underscore.
+        
+        Args:
+            text (str): The text to remove non-alphanumeric characters from.
+        """
         return re.sub(r'\W+', ' ', text)
     
     def remove_non_english_words(self, text:str) -> str:
+        """
+        Removes any non-English words from the text.
+        - Checks if the word is in the WordNet dictionary.
+        - Keeps single letter words if they are "a" or "i" or a number.
+        
+        Args:
+            text (str): The text to remove non-English words from.
+        """
         split_text = text.split(" ")
         english_words = []
         for word in split_text:
@@ -24,26 +50,37 @@ class TextPreprocessor:
                 if word_length > 1:
                     english_words.append(word)
                 else:
-                    # Keep single letter words if they are "a" or "i" or a number
                     is_numeric = word.isnumeric()
                     is_a_or_i = (word.lower() == "a" or word.lower() == "i")
                     if is_numeric or (word_length == 1 and is_a_or_i):
                         english_words.append(word)
-                    
         return " ".join(english_words)
     
     def extract_words(self, text:str) -> str:
+        """
+        Splits the text into words and re-joins them with spaces.
+        - Used to split words that are concatenated together.
+
+        Args:
+            text (str): The text to apply the operation to.
+        """
         all_words = wordninja.split(text)
         print(all_words)
         print(len(all_words))
         return " ".join(all_words)
     
     def __call__(self, text:str) -> str:
+        """
+        The main method that applies all the text processing operations to the input text.
+
+        Args:
+            text (str): The text to process.
+        """
         text = self.remove_newlines(text)
 
         operations = [
                     self.remove_links, 
-                    self.remove_special_characters, 
+                    self.keep_only_alphanumeric, 
                     self.extract_words,
                     self.remove_non_english_words,
                     ]
