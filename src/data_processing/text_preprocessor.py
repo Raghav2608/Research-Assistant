fa67fdd7086a02716613141b62b029b6f4ad7ff7
Gee-Lyle Wong <gee-lyle.wong@kcl.ac.uk>
2025-01-23 20:39:17 +0000

    feat: Added removal of non-english words.

--- src/data_processing/text_preprocessor.py
+++ src/data_processing/text_preprocessor.py
@@ -1,4 +1,7 @@
 import re
+import nltk
+from nltk.corpus import wordnet
+nltk.download('wordnet')
 
 class TextPreprocessor:
     def remove_newlines(self, text:str) -> str:
@@ -6,8 +9,18 @@ class TextPreprocessor:
     
     def remove_special_characters(self, text:str) -> str:
         return re.sub(r"[^a-zA-Z0-9\s`~!@#$%^&*()_+\-=\[\]{};:'\"\\|,.<>/?]", "", text)
-
+    
+    def remove_non_english_words(self, text:str) -> str:
+        split_text = text.split(" ")
+        english_words = [word for word in split_text if wordnet.synsets(word)]
+        return " ".join(english_words)
+    
     def __call__(self, text:str) -> str:
+        text = self.remove_newlines(text)
+
         text = self.remove_special_characters(text)
+        text = self.remove_newlines(text)
+
+        text = self.remove_non_english_words(text)
         text = self.remove_newlines(text)
         return text
