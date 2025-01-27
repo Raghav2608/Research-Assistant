d5465f4695bf88828c334d026c09b3465eb1702c
Gee-Lyle Wong <gee-lyle.wong@kcl.ac.uk>
2025-01-27 19:28:05 +0000

    feat: Added new process for extracting entities from text.

--- src/data_ingestion/arxiv/ner.py
+++ src/data_ingestion/arxiv/ner.py
@@ -1,14 +1,35 @@
-import spacy
+from transformers import T5ForConditionalGeneration, T5Tokenizer
 
 class NamedEntityRecognition:
     def __init__(self):
-        self.model = spacy.load("en_core_web_sm")
+        model_name = "google/flan-t5-base"
+        self.tokenizer = T5Tokenizer.from_pretrained(model_name, legacy=True)
+        self.model = T5ForConditionalGeneration.from_pretrained(model_name)
+
+    def create_prompt(self, sentence:str):
+        prompt = f"Extract the main topic that the user is asking about in the sentence: {sentence}"
+        return prompt
+    
+    def get_result(self, sentence):
+        prompt = self.create_prompt(sentence)
+        input_ids = self.tokenizer(prompt, return_tensors="pt").input_ids
+        output = self.model.generate(input_ids, max_length=50, num_beams=4, early_stopping=True)
+        key_phrases = self.tokenizer.decode(output[0], skip_special_tokens=True)
+        return key_phrases
 
     def __call__(self, sentences):
-        return sentences
+        entities_per_sentence = []
+        for sentence in sentences:
+            entities = self.get_result(sentence)
+            entities_per_sentence.append(entities)
+        return entities_per_sentence
 
 if __name__ == "__main__":
     ner = NamedEntityRecognition()
-    sentences = ["What are the latest advancements in deep learning"]
+    sentences = [
+                "What are the latest advancements in Computer Vision",
+                "What are the latest advancements in Artificial Intelligence",
+                "Is there anything new in Machine Learning that I should be aware of?"
+                ]
     out = ner(sentences)
     print("Output", out)
