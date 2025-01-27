a9443eb320107a1f6c450f46a2e22c2e9bbebf4e
Gee-Lyle Wong <gee-lyle.wong@kcl.ac.uk>
2025-01-27 19:41:44 +0000

    docs: Added documentation for the entity extraction class.

--- src/data_ingestion/arxiv/ner.py
+++ src/data_ingestion/arxiv/ner.py
@@ -1,28 +1,53 @@
 from transformers import T5ForConditionalGeneration, T5Tokenizer
-
+from typing import List
 class NamedEntityRecognition:
+    """
+    Class for extracting named entities (topics) from sentences using a pre-trained T5 model.
+    """
     def __init__(self):
         model_name = "google/flan-t5-large"
         self.tokenizer = T5Tokenizer.from_pretrained(model_name, legacy=True)
         self.model = T5ForConditionalGeneration.from_pretrained(model_name)
 
-    def create_prompt(self, sentence:str):
+    def create_prompt(self, sentence:str) -> str:
+        """
+        Creates a prompt, embedding the input sentence into it.
+
+        Args:
+            sentence (str): The sentence to embed into the prompt.
+        """
         prompt = f"Extract the main topic that the user is asking about in the sentence: {sentence}"
         return prompt
     
-    def get_result(self, sentence):
+    def get_result(self, sentence:str) -> str:
+        """
+        Finds the topic that the user is talking about inside of the sentence.
+        For example:
+        "What are the latest advancements in Computer Vision"
+        Should return:
+        "Computer Vision"
+
+        Args:
+            sentence (str): A sentence containing the topic to extract.
+        """
         prompt = self.create_prompt(sentence)
         input_ids = self.tokenizer(prompt, return_tensors="pt").input_ids
         output = self.model.generate(input_ids, max_length=50, num_beams=4, early_stopping=True)
         key_phrases = self.tokenizer.decode(output[0], skip_special_tokens=True)
         return key_phrases
 
-    def __call__(self, sentences):
-        entities_per_sentence = []
+    def __call__(self, sentences:List[str]) -> List[str]:
+        """
+        Processes a list of sentences and returns a list of topics for each sentence.
+
+        Args:
+            sentences (List[str]): A list of sentences to be processed.
+        """
+        topics_per_sentence = []
         for sentence in sentences:
-            entities = self.get_result(sentence)
-            entities_per_sentence.append(entities)
-        return entities_per_sentence
+            topics = self.get_result(sentence)
+            topics_per_sentence.append(topics)
+        return topics_per_sentence
 
 if __name__ == "__main__":
     ner = NamedEntityRecognition()
