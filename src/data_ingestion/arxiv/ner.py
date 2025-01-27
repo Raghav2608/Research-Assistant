d5d950f9a2bab9c1586917a5b1d8514cd5372484
Gee-Lyle Wong <gee-lyle.wong@kcl.ac.uk>
2025-01-27 19:35:24 +0000

    feat: Upgraded model for more accurate results.

--- src/data_ingestion/arxiv/ner.py
+++ src/data_ingestion/arxiv/ner.py
@@ -2,7 +2,7 @@ from transformers import T5ForConditionalGeneration, T5Tokenizer
 
 class NamedEntityRecognition:
     def __init__(self):
-        model_name = "google/flan-t5-base"
+        model_name = "google/flan-t5-large"
         self.tokenizer = T5Tokenizer.from_pretrained(model_name, legacy=True)
         self.model = T5ForConditionalGeneration.from_pretrained(model_name)
 
