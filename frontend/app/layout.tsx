6793da52b2feeb8bc90e51732e03d1de8bc40e4b
Talal <talal.abuzinadah@kcl.ac.uk>
2025-01-31 16:52:55 +0000

    Add basic layout for website

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,4 +1,3 @@
-import "./global.css";
 import React from "react";
 
 export default function RootLayout({
@@ -8,7 +7,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>My Research Assistant</title>
+      </head>
+      <body className="flex-col justify-between">
+        <header className="flex-row w-full bg-dark"><span>placeholder for logo</span></header>
+        <main className="flex-row w-full bg-light">{children}</main>
+        <footer className="flex-row w-full bg-dark"><span>Devloped by group 4 for LGP</span></footer>
+      </body>
     </html>
   );
 }
