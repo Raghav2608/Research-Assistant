78356de99ae4032f9c55f5137ddeef748a37aa18
Gee-Lyle Wong <gee-lyle.wong@kcl.ac.uk>
2025-02-21 18:45:28 +0000

    feat: Added recording time taken for data fetching.

--- fetch_data.py
+++ fetch_data.py
@@ -5,9 +5,10 @@ Small script for demonstrating the data ingestion pipeline for arXiv research pa
 from src.data_ingestion.arxiv.utils import fetch_arxiv_papers, parse_papers
 from src.data_ingestion.arxiv.topic_extractor import TopicExtractor
 from src.data_processing.entry_processor import EntryProcessor
+import time
 
 if __name__ == "__main__":
-
+    
     test_sentence = "Are there any recent advancements in transformer models?"
     topic_extractor = TopicExtractor()
     topic = topic_extractor([test_sentence])[0]
@@ -17,6 +18,7 @@ if __name__ == "__main__":
     start = 0
     max_results = 4
 
+    start_time = time.perf_counter()
     xml_papers = fetch_arxiv_papers(search_query, start, max_results)
     entries = parse_papers(xml_papers)
     entry_processor = EntryProcessor()
@@ -32,3 +34,6 @@ if __name__ == "__main__":
         print(processed_content)
         print("Number of characters (after processing):", len(processed_content))
         print("\n")
+
+    end_time = time.perf_counter()
+    print(f"Time taken: {end_time-start_time:.2f} seconds")
