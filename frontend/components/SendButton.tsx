d8ed1fe727592c3d37b23da8f58ad64417042168
Talal <talal.abuzinadah@kcl.ac.uk>
2025-03-12 00:48:41 +0000

    Change send button to icon

--- frontend/components/SendButton.tsx
+++ frontend/components/SendButton.tsx
@@ -1,4 +1,5 @@
 import { Dispatch, SetStateAction } from "react";
+import { Send } from "lucide-react";
 
 export interface SendButtonProps {
   chatInput: string;
@@ -11,8 +12,11 @@ export default function SendButton({
   send,
 }: SendButtonProps) {
   return (
-    <button onClick={send} className="w-20 bg-primary rounded-full m-2 h-3/4 hover:bg-dark hover:text-lightest">
-      <span className="text-l text-lightest">Send</span>
+    <button
+      onClick={send}
+      className="w-20 bg-primary rounded-full m-2 h-3/4 hover:bg-dark hover:text-lightest flex items-center justify-center "
+    >
+      <Send className="w-5 h-5" />
     </button>
   );
 }
