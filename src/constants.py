c1f203521436d75871771249d7f55bb47fc08700
Gee-Lyle Wong <gee-lyle.wong@kcl.ac.uk>
2025-03-13 17:46:59 +0000

    feat: Added file for constants and added file for web-app endpoint.

---
+++ src/constants.py
@@ -0,0 +1,10 @@
+ENDPOINT_URLS = {
+    "web_app": {
+        "base_url": "localhost:8000",
+        "app_name": "app_webapp",
+        "path": "/",
+        "additional_paths": {
+            "query": "/query"
+        }
+    }
+}
