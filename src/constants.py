597eb6e6559a75510e3e5322c7653f3846a6af10
Gee-Lyle Wong <gee-lyle.wong@kcl.ac.uk>
2025-03-13 17:59:31 +0000

    feat: Added data ingestion endpoint.

--- src/constants.py
+++ src/constants.py
@@ -6,5 +6,10 @@ ENDPOINT_URLS = {
         "additional_paths": {
             "query": "/query"
         }
+    },
+    "data_ingestion": {
+        "base_url": "localhost:8001",
+        "app_name": "app_data_ingestion",
+        "path": "/data_ingestion",
     }
 }
