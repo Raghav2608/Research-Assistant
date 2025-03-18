c2da37a4f639f78b36e7c2d8eb243518003c2895
Gee-Lyle Wong <gee-lyle.wong@kcl.ac.uk>
2025-03-18 22:20:28 +0000

    feat: Added monitoring for system calls.

--- test_system.py
+++ test_system.py
@@ -1,14 +1,17 @@
 import requests
+import time
 
 from backend.src.constants import ENDPOINT_URLS
 
 if __name__ == "__main__":
-
     BASE_WEBAPP_URL = "http://localhost:8000"
     SYSTEM_URL = BASE_WEBAPP_URL + ENDPOINT_URLS['web_app']['additional_paths']['query']
     payload = {"user_query": "Are there any recent advancements in transformer models?"}
-    
+
+    start_time = time.perf_counter()
     response = requests.post(SYSTEM_URL, json=payload)
+    end_time = time.perf_counter()
 
     print(f"Status code: {response.status_code}")
     print(f"Response: {response.json()}")
+    print(f"API call time: {end_time-start_time:.5f} seconds")
