2359ca007ce67962b575f1b97893324ddfd04051
Gee-Lyle Wong <gee-lyle.wong@kcl.ac.uk>
2025-03-22 19:14:27 +0000

    feat: Added query processing that is compatible with arXiv + SS data ingestion.

--- backend/src/data_ingestion/data_pipeline.py
+++ backend/src/data_ingestion/data_pipeline.py
@@ -23,6 +23,7 @@ class DataPipeline:
         # ADD DATA INGESTION PIPELINES HERE:
         self.arxiv_data_ingestion_pipeline = ArXivDataIngestionPipeline()
         self.ss_data_ingestion_pipeline = SSDataIngestionPipeline()
+        self.text_preprocessor = self.data_processing_pipeline.entry_processor.text_preprocessor
 
         #########################################
         #########################################
@@ -30,6 +31,26 @@ class DataPipeline:
         self.max_total_entries = max_total_entries
         self.min_entries_per_query = min_entries_per_query
 
+    def process_query(self, user_query:str) -> str:
+        """
+        Processes the user query by removing non-alphanumeric characters, stopwords
+        and formats the query to be used for fetching data.
+
+        Args:
+            user_query (str): The user query to process.
+        """
+        processed_query = self.text_preprocessor.keep_only_alphanumeric(user_query)
+        processed_query = self.text_preprocessor.remove_newlines(processed_query)
+        print(processed_query)
+
+        processed_query = self.text_preprocessor.remove_stopwords(processed_query)
+        processed_query = self.text_preprocessor.remove_newlines(processed_query)
+        print(processed_query)
+        
+        processed_query = clean_search_query(processed_query)
+        print(processed_query)
+        return processed_query
+    
     def run(self, user_queries:List[str]) -> List[Dict[str, Any]]:
         """
         Fetches data from various sources using the user queries and processes
@@ -43,35 +64,37 @@ class DataPipeline:
         # Fetch entries from all data ingestion pipelines
         for query in user_queries:
             remaining_entries_left = self.max_total_entries - len(all_entries)
-            # arxiv_entries = self.arxiv_data_ingestion_pipeline.fetch_entries(
-            #                                                                 topic=clean_search_query(query), 
-            #                                                                 max_results=min(
-            #                                                                                 self.min_entries_per_query, 
-            #                                                                                 remaining_entries_left
-            #                                                                                 ) + 1 # +1 as this is non-inclusive
-            #                                                                 )
-            # TODO: Implement cleaning search query for semantic scholar as queries do not work as expected.
-            ss_entries = self.ss_data_ingestion_pipeline.get_entries(
-                                                                    topic=query, 
-                                                                    max_results=20, # Get 20, but only use "desired_total" number of entries
-                                                                    desired_total=remaining_entries_left
-                                                                    )                                                         
+            processed_query = self.process_query(query)
+
+            arxiv_entries = self.arxiv_data_ingestion_pipeline.fetch_entries(
+                                                                            topic=processed_query, 
+                                                                            max_results=min(
+                                                                                            self.min_entries_per_query, 
+                                                                                            remaining_entries_left
+                                                                                            ) + 1 # +1 as this is non-inclusive
+                                                                            )
+
+            # ss_entries = self.ss_data_ingestion_pipeline.get_entries(
+            #                                                         topic=processed_query, 
+            #                                                         max_results=20, # Get 20, but only use "desired_total" number of entries
+            #                                                         desired_total=remaining_entries_left
+            #                                                         )                                                         
 
             # ADD MORE DATA INGESTION PIPELINES HERE:
             #########################################
             #########################################
 
 
-            # # Add entries from all data ingestion pipelines into a single list
-            # for entry in arxiv_entries:
-            #     if len(all_entries) >= self.max_total_entries:
-            #         break
-            #     all_entries.append(entry)
-
-            for entry in ss_entries:
+            # Add entries from all data ingestion pipelines into a single list
+            for entry in arxiv_entries:
                 if len(all_entries) >= self.max_total_entries:
                     break
                 all_entries.append(entry)
+
+            # for entry in ss_entries:
+            #     if len(all_entries) >= self.max_total_entries:
+            #         break
+            #     all_entries.append(entry)
 
         # Process all entries
         all_entries = self.data_processing_pipeline.process(all_entries)
--- test_dataPipeline.py
+++ test_dataPipeline.py
@@ -1,15 +1,28 @@
+import os
+
 from backend.src.data_ingestion.data_pipeline import DataPipeline
+from backend.src.RAG.query_generator import ResearchQueryGenerator
 
 def main():
+    OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+    query_generator = ResearchQueryGenerator(openai_api_key=OPENAI_API_KEY,session_id="foo")
+
     # Define a sample query. You can change this query as needed.
-    sample_query = "GANs in machine learning"
-    print("Testing data pipeline with query:", sample_query)
+    sample_queries = [
+        # "GANS in machine learning",
+        # "Are there any recent advancements in transformer models?",
+        # "What are the applications of transformer models in recent research?",
+        "I am researching how deep learning can be applied to the early detection of neurodegenerative diseases such as Alzheimer's and Parkinson's. I am particularly interested in papers that discuss convolutional neural networks (CNNs) or transformer-based models for medical image analysis, especially MRI or CT scans. The papers should focus on explainability and feature extraction techniques rather than just model performance."
+    ]
+
+    additional_queries = query_generator.generate(sample_queries[0])
+    print(additional_queries)
     
     # Create an instance of the DataPipeline
     pipeline = DataPipeline()
     
     # Run the pipeline with the sample query
-    results = pipeline.run([sample_query])
+    results = pipeline.run(additional_queries)
     
     # Print the processed results
     print("Processed entries:")
