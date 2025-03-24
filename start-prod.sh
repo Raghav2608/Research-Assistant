0368c89ee3c33252db704d4ac69256a9c4465636
Talal <talal.abuzinadah@kcl.ac.uk>
2025-03-24 23:16:52 +0000

    Tweak build for deployment

--- start-prod.sh
+++ start-prod.sh
@@ -2,6 +2,14 @@
 # Start frontend service
 npm run build --prefix ./frontend
 npm run start --prefix ./frontend&
+
+# Wait until the frontend is available on port 3000
+while ! nc -z localhost 3000; do   
+  echo "Waiting for frontend to start..."
+  sleep 1
+done
+
+echo "Frontend is up. Starting backend services..."
 # Start backend services on different ports
 uvicorn backend.apps.app_webapp:app --host 0.0.0.0 --port 8000 &
 uvicorn backend.apps.app_data_ingestion:app --host 0.0.0.0 --port 8001 &
