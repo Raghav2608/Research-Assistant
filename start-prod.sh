50137c56870a59a09a2b2838de34e79d3a283475
Talal <talal.abuzinadah@kcl.ac.uk>
2025-03-25 01:01:43 +0000

    Add backend checking when deployment

--- start-prod.sh
+++ start-prod.sh
@@ -4,7 +4,7 @@ npm run build --prefix ./frontend
 npm run start --prefix ./frontend&
 
 # Wait until the frontend is available on port 3000
-while ! nc -z localhost 3000; do   
+until curl --silent --fail http://localhost:3000 > /dev/null; do
   echo "Waiting for frontend to start..."
   sleep 1
 done
@@ -16,4 +16,16 @@ uvicorn backend.apps.app_data_ingestion:app --host 0.0.0.0 --port 8001 &
 uvicorn backend.apps.app_retrieval:app --host 0.0.0.0 --port 8002 &
 uvicorn backend.apps.app_llm_inference:app --host 0.0.0.0 --port 8003 &
 # Wait for all background processes to exit
+
+# Wait until all backend /docs endpoints are available
+ports=(8000 8001 8002 8003)
+for i in "${ports[@]}"; do
+  until curl --silent --fail http://localhost:$i/docs > /dev/null; do
+    echo "Waiting for backend service on port $i..."
+    sleep 1
+  done
+done
+
+echo "All backend services are up and running."
+
 wait
