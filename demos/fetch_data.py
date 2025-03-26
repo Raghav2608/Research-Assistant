ce2ab311aa222ee191a61646f596a37a494cb046
wlyle473 <wlyle473@gmail.com>
2025-03-26 14:43:38 +0000

    fix: Issue with 'fetch_data' script not working as intended.

--- demos/fetch_data.py
+++ demos/fetch_data.py
@@ -1,13 +1,14 @@
 """
 Small script for how to use the data pipeline to fetch and process papers from arXiv and other source.
 """
+import set_path
 
 from backend.src.data_ingestion.data_pipeline import DataPipeline
 if __name__ == "__main__":
     
     test_sentence = "Are there any recent advancements in transformer models?"
     data_pipeline = DataPipeline()
-    entries = data_pipeline.run(test_sentence)
+    entries = data_pipeline.run(user_queries=[test_sentence])
 
     print(type(entries))
     
